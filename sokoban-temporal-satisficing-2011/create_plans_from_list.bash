e83f9ed17100b8d7e7b1598df016522cab947daa
Andrew Murray <kpb20194@pd-michae2.cis.strath.ac.uk>
2022-11-01 19:23:39 +0000

    deleted local scripts for running plans

--- sokoban-temporal-satisficing-2011/create_plans_from_list.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-path=$(pwd)
-domain=$path/domain.pddl
-input=$path/to_run.txt
-
-while IFS= read -r file; do
-    echo "Solving Problem ${FILE}";
-    timeout 600 optic-cplex $domain ${path}/instances/${file} > ${path}/plans/sokoban_${file%.*}_plan.pddl;
-done < "$input"
